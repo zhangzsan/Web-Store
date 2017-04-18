1e0219d7f3244730336b71e50088bb3c20995928
Viktor Savonin <viktor.savonin@meawallet.com>
2017-04-18 17:08:23 +0300

    ProductRegistrationServiceImplTest

--- src/main/java/lv/javaguru/java2/services/product/ProductRegistrationServiceImpl.java
+++ src/main/java/lv/javaguru/java2/services/product/ProductRegistrationServiceImpl.java
@@ -1,7 +1,6 @@
 package lv.javaguru.java2.services.product;
 
 import lv.javaguru.java2.database.ProductDAO;
-import lv.javaguru.java2.database.jdbc.ProductDAOImpl;
 import lv.javaguru.java2.domain.Product;
 
 import static lv.javaguru.java2.domain.ProductBuilder.createProduct;
@@ -10,11 +9,20 @@ import static lv.javaguru.java2.domain.ProductBuilder.createProduct;
  * Created by Aster on 05.04.2017.
  */
 public class ProductRegistrationServiceImpl implements ProductRegistrationService {
-    private ProductValidator productValidator = new ProductValidatorImpl();
-    private ProductDAO productDAO = new ProductDAOImpl();
+
+    private ProductValidator productValidator;
+    private ProductDAO productDAO;
+
+    public ProductRegistrationServiceImpl(ProductValidator productValidator,
+                                          ProductDAO productDAO) {
+        this.productValidator = productValidator;
+        this.productDAO = productDAO;
+    }
 
     @Override
-    public Product register(String productName, String productCategory, Double price)  {
+    public Product register(String productName,
+                            String productCategory,
+                            Double price)  {
             productValidator.validate(productName, productCategory, price);
 
             Product product = createProduct()
